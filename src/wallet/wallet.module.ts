90710a386ca6bb91aad0a4033a659bf1ae81cb2c
Barny Victor <victorbarny4@gmail.com>
2023-05-16 10:26:45 +0100

    deleted the dist

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -1,13 +1,13 @@
 import { Module, forwardRef } from '@nestjs/common';
 import WalletService from './wallet.service';
 import { UserModule } from '../user/user.module';
+import { TransactionModule } from '../transaction/transaction.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../user/user.entity';
-import { TransactionEntity } from 'src/Transaction/transaction.entity';
+import { TransactionEntity } from '../Transaction/transaction.entity';
 import { AuthModule } from '../auth/auth.module';
 import { Wallet } from './wallet.entity';
 import { WalletController } from './wallet.controller';
-import { TransactionModule } from 'src/transaction/transaction.module';
 
 @Module({
   imports: [
@@ -20,6 +20,5 @@ import { TransactionModule } from 'src/transaction/transaction.module';
   ],
   providers: [WalletService],
   controllers: [WalletController],
-  exports: [WalletService],
 })
 export class WalletModule {}
